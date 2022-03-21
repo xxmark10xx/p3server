eded7a967ca65c7e63114b1303c936fe63e43f6d
weston-bailey <wdbailey87@gmail.com>
2022-03-21 16:54:01 -0700

    finish day

--- index.js
+++ index.js
@@ -10,8 +10,19 @@ const PORT = process.env.PORT || 3001
 app.use(cors())
 app.use(express.json())
 
+const myMiddleWare = (req, res, next) => {
+  console.log(`incoming request: ${req.method} - ${req.url}`)
+  // move along there
+  next()
+}
+
+app.use(myMiddleWare)
+
 app.get('/', (req, res) => {
   res.json({ msg: 'welcome to the user app 👋' })
 })
+
+// controllers
+app.use('/api-v1/users', require('./controllers/api-v1/users'))
 
 app.listen(PORT, () => console.log(`listening to the smooth sounds of port ${PORT} in the morning 🌊`))
