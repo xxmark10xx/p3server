093a6800221213c7787c5b8e57beae752a55b017
xxmark10xx <mark.a.aguilar99@gmail.com>
2022-03-25 10:48:21 -0700

    moved position of controllers in index

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ const myMiddleWare = (req, res, next) => {
 
 app.use(myMiddleWare)
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'welcome to the user app 👋' })
-})
-
 // controllers
 app.use('/api-v1/users', require('./controllers/api-v1/users'))
 app.use('/api-v1/profile', require('./controllers/api-v1/profile'))
 app.use('/api-v1/rooms', require('./controllers/api-v1/rooms'))
 app.use('/api-v1/timeline', require('./controllers/api-v1/timeline'))
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'welcome to the user app 👋' })
+})
 
 app.listen(PORT, () => console.log(`listening to the smooth sounds of port ${PORT} in the morning 🌊`))
