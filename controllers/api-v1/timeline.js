527a438499ddc7b84ad4fa7a46b108ea70750bfd
romanprotoliuk <romanprotoliuk@gmail.com>
2022-03-25 10:33:20 -0700

    Timeline basic routes

--- controllers/api-v1/timeline.js
+++ controllers/api-v1/timeline.js
@@ -1,3 +1,19 @@
-const router = require("express").Router()
+const router = require('express').Router();
 
-module.exports = router
+// GET /timeline -> view main timeline chatroom
+router.get('/', (req, res) => {
+	res.json({ msg: 'main timeline chatroom' });
+});
+
+// POST /timeline -> add a chat to the timeline
+router.post('/', (req, res) => {
+	res.json({ msg: 'add chat timeline' });
+});
+
+// DELETE /timeline -> Remove your own
+router.delete('/:messageId', (req, res) => {
+	let pId = req.params.messageId;
+	res.json({ msg: `remove your own message:${pId} from the timeline ` });
+});
+
+module.exports = router;
