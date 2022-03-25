dbba02451dc812fb2abdab456e19b459a5d338ed
snacksident <leak187@gmail.com>
2022-03-25 11:08:00 -0700

    add timestamp to all models

--- models/message.js
+++ models/message.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 
 const MessageSchema = mongoose.Schema({
   content: String
-})
+},{timestamps: true})
 
 module.exports = mongoose.model('Message', MessageSchema)
