20d98f2d72b55101bba83146dbb1cb1354e7062e
romanprotoliuk <romanprotoliuk@gmail.com>
2022-03-25 14:18:49 -0700

    Image upload feature

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-require('dotenv').config()
-require('./models')
-const express = require('express')
-const cors = require('cors')
+require('dotenv').config();
+require('./models');
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // middlewares
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const myMiddleWare = (req, res, next) => {
-  console.log(`incoming request: ${req.method} - ${req.url}`)
-  // move along there
-  next()
-}
+	console.log(`incoming request: ${req.method} - ${req.url}`);
+	// move along there
+	next();
+};
 
-app.use(myMiddleWare)
+app.use(myMiddleWare);
 
 app.get('/', (req, res) => {
-  res.json({ msg: 'welcome to the user app 👋' })
-})
+	res.json({ msg: 'welcome to the user app 👋' });
+});
 
 // controllers
-app.use('/api-v1/users', require('./controllers/api-v1/users'))
-app.use('/api-v1/profile', require('./controllers/api-v1/profile'))
-app.use('/api-v1/rooms', require('./controllers/api-v1/rooms'))
-app.use('/api-v1/timeline', require('./controllers/api-v1/timeline'))
+app.use('/api-v1/users', require('./controllers/api-v1/users'));
+app.use('/api-v1/profile', require('./controllers/api-v1/profile'));
+app.use('/api-v1/rooms', require('./controllers/api-v1/rooms'));
+app.use('/api-v1/timeline', require('./controllers/api-v1/timeline'));
+app.use('/api-v1/images', require('./controllers/api-v1/images'));
 
-app.listen(PORT, () => console.log(`listening to the smooth sounds of port ${PORT} in the morning 🌊`))
+app.listen(PORT, () => console.log(`listening to the smooth sounds of port ${PORT} in the morning 🌊`));
