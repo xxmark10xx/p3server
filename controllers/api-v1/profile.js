f35d540e407f2bd7b4d4730f12685728ef750036
xxmark10xx <mark.a.aguilar99@gmail.com>
2022-03-25 14:45:36 -0700

    planning routes

--- controllers/api-v1/profile.js
+++ controllers/api-v1/profile.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
         })
         if(foundUser){
             await db.User.find({
-                name: 
+                
             })
         }
     }catch(err) {
