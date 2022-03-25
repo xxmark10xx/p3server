706f25974dc7e7812f74b4be4c98bb1efc613bec
xxmark10xx <mark.a.aguilar99@gmail.com>
2022-03-25 10:32:15 -0700

    making profile routes for postman

--- controllers/api-v1/profile.js
+++ controllers/api-v1/profile.js
@@ -1,3 +1,24 @@
 const router = require("express").Router()
+const db = require("../../models")
+
+// GET -- /profile display user profile
+router.get("/", (req, res) => {
+    res.send("Hello from profile")
+})
+
+// GET -- /profile/:id displays specific user 
+router.get("/:id", (req, res) => {
+    res.send("Specific User")
+})
+
+// PUT -- /profile/:id update personal
+router.get("/:id", (req, res) => {
+    res.send("Updating the user")
+})
+
+// DELETE -- /profile/:id
+router.get("/:id", (req, res) => {
+    res.send("Deleting the user")
+})
 
 module.exports = router
