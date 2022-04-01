e96e585abbc3009d36d3326c92e9a395e1fc289f
romanprotoliuk <romanprotoliuk@gmail.com>
2022-03-31 22:22:49 -0700

    change avatar

--- models/user.js
+++ models/user.js
@@ -1,18 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const UserSchema = mongoose.Schema({
-  name: String,
-  email: String,
-  username: String,
-  password: String,
-  online: Boolean,
-  avatar: {type: String, default: `https://res.cloudinary.com/dtmyibw85/image/upload/v1648664128/defaultuser_rhnnd6.png`},
-  chatrooms: [{
-    type: mongoose.Schema.Types.ObjectId, ref: 'Chatroom'
-  }],
-  messages: [{
-    type: mongoose.Schema.Types.ObjectId, ref: 'Message'
-  }]
-},{timestamps: true})
+const UserSchema = mongoose.Schema(
+	{
+		name: String,
+		email: String,
+		username: String,
+		bio: String,
+		password: String,
+		online: Boolean,
+		avatar: {
+			type: String,
+			default: `https://uploads-ssl.webflow.com/60f4af788dc31bdb2d5c8b29/62468ba5f726d384994823a6_img-user.png`
+		},
+		chatrooms: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Chatroom'
+			}
+		],
+		messages: [
+			{
+				type: mongoose.Schema.Types.ObjectId,
+				ref: 'Message'
+			}
+		]
+	},
+	{ timestamps: true }
+);
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model('User', UserSchema);
